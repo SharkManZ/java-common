10aa8b5182e384be520e329d475345a52c9c141a
SharkManZ <DG5BZ2TL>
2021-11-05 20:53:30 +0300

    добавлена dto для перечислений

---
+++ src/main/java/ru/shark/home/common/dao/dto/EnumDto.java
@@ -0,0 +1,31 @@
+package ru.shark.home.common.dao.dto;
+
+public class EnumDto {
+    private String code;
+    private String title;
+
+    public EnumDto() {
+        // empty constructor
+    }
+
+    public EnumDto(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
