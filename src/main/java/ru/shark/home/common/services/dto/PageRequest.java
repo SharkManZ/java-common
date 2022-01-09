e6e08c2dcdce02996459db00ad98c352d3f7678d
SharkManZ <DG5BZ2TL>
2022-01-09 22:24:40 +0300

    Добавлена dto для фильтрации, поиска и сортировки без пагинации.

--- src/main/java/ru/shark/home/common/services/dto/PageRequest.java
+++ src/main/java/ru/shark/home/common/services/dto/PageRequest.java
@@ -1,13 +1,9 @@
 package ru.shark.home.common.services.dto;
 
-import java.util.List;
-
-public class PageRequest {
+public class PageRequest extends ListRequest {
     private int page;
     private int size;
-    private List<ru.shark.home.common.services.dto.Filter> filters;
-    private Search search;
-    private List<ru.shark.home.common.services.dto.Sort> sorts;
+
 
     public PageRequest() {
         // empty constructor
@@ -32,29 +28,5 @@ public class PageRequest {
 
     public void setSize(int size) {
         this.size = size;
-    }
-
-    public List<ru.shark.home.common.services.dto.Filter> getFilters() {
-        return filters;
-    }
-
-    public void setFilters(List<ru.shark.home.common.services.dto.Filter> filters) {
-        this.filters = filters;
-    }
-
-    public Search getSearch() {
-        return search;
-    }
-
-    public void setSearch(Search search) {
-        this.search = search;
-    }
-
-    public List<ru.shark.home.common.services.dto.Sort> getSorts() {
-        return sorts;
-    }
-
-    public void setSorts(List<ru.shark.home.common.services.dto.Sort> sorts) {
-        this.sorts = sorts;
     }
 }
