83d7a2215e49116a166a30b60d5db9ceb8ff9341
SharkManZ <DG5BZ2TL>
2022-02-12 23:27:03 +0300

    Добавлена константа

--- src/main/java/ru/shark/home/common/common/ErrorConstants.java
+++ src/main/java/ru/shark/home/common/common/ErrorConstants.java
@@ -18,6 +18,7 @@ public class ErrorConstants {
     public static final String ENTITY_ALREADY_EXISTS = "Сущность \"{0}\" с ключом \"{1}\" уже существует";
     public static final String ENTITY_NOT_FOUND_BY_ID = "Сущность \"{0}\" с идентификатором {1} не найдена";
     public static final String ENTITY_EMPTY_FIELD = "Поле {0} сущности {1} не задано";
+    public static final String ENTITY_FIELD_VALUE_LOWER = "Значение поля \"{0}\" должно быть больше {1}";
 
     public static final String JSON_PROCESS_ERROR = "Ошибка преобразования объекта в JSON";
     public static final String OBJECTS_TO_ZIP_ERROR = "Ошибка записи списка объектов в zip";
